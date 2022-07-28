0db8212e22fdcbe61d0edf413ea5ea3484c43397
camlo01 <camilobeltran174@gmail.com>
2022-07-28 14:58:18 -0500

    added loggers to indicated what the initialization was successful

--- src/main/java/com/softka/challenge/ChallengeApplication.java
+++ src/main/java/com/softka/challenge/ChallengeApplication.java
@@ -1,13 +1,26 @@
 package com.softka.challenge;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeApplication {
+public class ChallengeApplication implements CommandLineRunner {
+
+    private final Log Logger = LogFactory.getLog(ChallengeApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ChallengeApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Logger.info("-----------------------------");
+        Logger.info("INITIALIZED SUCCESSFULLY! :D ");
+        Logger.info("-----------------------------");
+    }
 }
