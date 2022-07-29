241cbb5138a49d9c42339848a73e1be6202a3dc7
camlo01 <camilobeltran174@gmail.com>
2022-07-28 23:42:22 -0500

    implemented services layer

---
+++ src/main/java/com/softka/challenge/services/SpacecraftServices.java
@@ -0,0 +1,30 @@
+package com.softka.challenge.services;
+
+import com.softka.challenge.Repository.spacecraftRepository;
+import com.softka.challenge.model.entity.spacecraft.Spacecraft;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageTranscoder;
+import java.util.List;
+
+@Service
+public class SpacecraftServices {
+
+    @Autowired
+    private spacecraftRepository repository;
+
+    //GET
+
+    public Iterable<Spacecraft> allShips(){
+        return repository.findAll();
+    }
+
+    //POST
+
+    public Spacecraft save(Spacecraft ship){
+        return repository.save(ship);
+    }
+
+
+}
