64a19b23fa5e6a2d1bcd03dfaeaf396cac3bfa88
camlo01 <camilobeltran174@gmail.com>
2022-07-29 23:46:44 -0500

    controller layer implemented

--- src/main/java/com/softka/challenge/controller/SpacecraftController.java
+++ src/main/java/com/softka/challenge/controller/SpacecraftController.java
@@ -4,6 +4,7 @@ package com.softka.challenge.controller;
 import com.softka.challenge.model.entity.spacecraft.Spacecraft;
 import com.softka.challenge.services.SpacecraftServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,25 +12,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/ship")
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET
-//        , RequestMethod.POST, RequestMethod.PUT,
-//        RequestMethod.DELETE
-})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class SpacecraftController {
 
     @Autowired
     private SpacecraftServices services;
 
+    //--GET
+
     @GetMapping("/all")
-    public @ResponseBody
-    Iterable<Spacecraft> getAllShips() {
+    public Iterable<Spacecraft> getAllShips() {
         return services.allShips();
     }
 
+    @GetMapping("/search={parameter}")
+    public Iterable<Spacecraft> searchShip(@PathVariable("parameter") String parameter) {
+        return services.searchBy(parameter);
+    }
+
+    //--POST
+
     @PostMapping("/save")
     public @ResponseBody
-    Spacecraft save(@RequestBody Spacecraft ship){
+    Spacecraft save(@RequestBody Spacecraft ship) {
+        System.out.println(ship.getName());
         return services.save(ship);
     }
+
+
 
 }
